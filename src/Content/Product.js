71cb5124fb008b002618da5a82726f5c6696a4d0
aarya moktan <aaryamoktan2055@gmail.com>
2023-09-02 00:06:38 -0700

    commit first

--- src/Content/Product.js
+++ src/Content/Product.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react'
+import { Chart as chartjs,Bar } from 'react-chartjs-2'
 
 const Product = () => {
   const [data,setdata] =useState("")
   const [final,setfinal] = useState([])
+
+const data1 = {
+  labels :  ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};
+ 
   const submit = (e)=>
   {
     e.preventDefault();
@@ -16,6 +30,7 @@ const Product = () => {
         return [...old]
     }
       
+
     })
     setdata("")
   }
@@ -33,6 +48,7 @@ const Product = () => {
   console.log(final.length)
   return (
     <><div className="addProduct">
+    <Bar data1={data}/>
       <input type="text" value={data} onChange={(e)=>
       {
         setdata(e.target.value)
